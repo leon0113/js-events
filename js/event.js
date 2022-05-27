a279ea50d785a5f6b6ce03d23c084bb2e6474f47
leon0113 <tahjib.leon399@gmail.com>
2022-05-28 02:23:53 +0600

    adding event in different ways

--- js/event.js
+++ js/event.js
@@ -10,3 +10,26 @@ function makepink() {
     document.body.style.backgroundColor = 'pink';
     document.body.style.color = 'gray';
 }
+
+//  Using Anonymous function
+const greenButton = document.getElementById('make-green-button');
+greenButton.onclick = function () {
+    document.body.style.backgroundColor = 'green';
+    document.body.style.color = 'lightblue';
+}
+
+
+// Using addEventListner 
+const goldenRodButton = document.getElementById('make-goldenRod');
+goldenRodButton.addEventListener('click', makeGoldenRod);
+function makeGoldenRod() {
+    document.body.style.backgroundColor = 'goldenrod';
+
+}
+
+// addEventListner in shortcut
+const makeSlateBlue = document.getElementById('makeSlateBlue');
+makeSlateBlue.addEventListener('click', function () {
+    document.body.style.backgroundColor = 'SlateBlue';
+    document.body.style.color = 'white';
+});
